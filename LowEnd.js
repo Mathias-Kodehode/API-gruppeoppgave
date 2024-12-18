06edf466b96a88a31196068db846bbd2427987c2
Gabriel-Kodehode <gabriel.kodehode@gmail.com>
2024-12-18 10:29:53 +0100

    fix Low end planet not loading

--- LowEnd.js
+++ LowEnd.js
@@ -7,14 +7,16 @@ const containerWidth = 350;
 window.addEventListener("keydown", (event) => {
   if (event.key === "ArrowRight") {
     position -= scrollSpeed;
+    console.log(position);
   } else if (event.key === "ArrowLeft") {
     position += scrollSpeed;
+    console.log(position);
   }
 
   if (position <= -containerWidth) {
     position = 0;
-  } else if (position >= containerWidth) {
-    position = 0;
+  } else if (position >= containerWidth - 50) {
+    position = -50;
   }
 
   planetWrapper.style.transform = `translateX(${position}px)`;
